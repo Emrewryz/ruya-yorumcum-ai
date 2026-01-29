84f7c33d657be5d4f0b1baa4f6c560ecba43ca43
ZyzzEmre <fikriemretopcu07@gmail.com>
2026-01-29 16:28:51 +0300

    guncellemesi

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    // Uyarıları hata olarak görüp build'i durdurmasın
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // Tip hatalarını görmezden gelsin (Hızlı deploy için)
-    ignoreBuildErrors: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org', // <-- Bunu mutlaka ekle!
+      },
+    ],
   },
 };
 
